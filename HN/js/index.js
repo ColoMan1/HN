2ebc3aa2aef7889286247f7aad26e6f0b7637069
ColoMan1 <895843172@qq.com>
2017-03-15 18:00:41 +0800

    页面3.15

--- HN/js/index.js
+++ HN/js/index.js
@@ -1,11 +1,16 @@
-$("h1").click(function(){
-	alert(1);
+
+$(function(){
+		var $a = $('.cantent_box ul li a');
+		var $box = $('.bigbox .brand_cantent_box');
+		$box.eq(0).show().siblings().hide();
+	$a.click(function(){
+		$(".cantent_box ul li a:first-child").removeClass("active_a");
+		var $index = $a.index(this);
+		console.log($index);
+    	$box.eq($index).show(500).siblings().hide(500);
+	})
+
 })
-
-
-
-
-
 
 
 
