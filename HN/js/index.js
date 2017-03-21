e2dbe64684486c44e4a91da282db8aa31918c86c
ColoMan1 <895843172@qq.com>
2017-03-21 17:42:36 +0800

    3/21     17:41   今天要完成联系我们

--- HN/js/index.js
+++ HN/js/index.js
@@ -28,11 +28,39 @@ $(function(){
 	var $li = $('.yuan_box ul li');	
 
 	$($li).click(function(event) {
+		$('.home_animated1').removeClass('animated flip');
+		$('.home_animated2').removeClass('animated swing');
+		$('.home_animated3').removeClass('animated bounceInLeft');
+		$('.home_animated4').removeClass('animated lightSpeedIn');
+		$('.home_animated5').removeClass('animated bounceInLeft');
+		$('.home_animated6').removeClass('animated lightSpeedIn');
+		$('.home_animated7').removeClass('animated rotateInDownLeft');
+		$('.home_animated8').removeClass('animated rotateInDownRight');
+		$('.home_animated9').removeClass('animated rotateInDownLeft');
+		$('.home_animated10').removeClass('animated rotateInDownRight');
+		$('.home_animated11').removeClass('animated wobble');
+		
 		var $index = $li.index(this);
 		console.log($index);
 		if( $index == 1){
-			$('.three_wenzi p').addClass('animated bounceInLeft');
-			console.log($index)
+			$('.home_animated1').addClass('animated flip');
+			$('.home_animated2').addClass('animated swing');
+
+		};
+		if($index == 2){
+			$('.home_animated3').addClass('animated bounceInLeft');
+			$('.home_animated4').addClass('animated lightSpeedIn');
+			$('.home_animated5').addClass('animated bounceInLeft');
+			$('.home_animated6').addClass('animated lightSpeedIn');			
+		};
+		if( $index == 3){
+			$('.home_animated7').addClass('animated rotateInDownLeft');			
+			$('.home_animated8').addClass('animated rotateInDownRight');			
+			$('.home_animated9').addClass('animated rotateInDownLeft');			
+			$('.home_animated10').addClass('animated rotateInDownRight');			
+		};
+		if($index == 4){
+			$('.home_animated11').addClass('animated wobble');
 		};
 	});
 
